b83fee8e9f9d2ac58726424732bccfc7766425bd
tomasgiordano <47353437+tomasgiordano@users.noreply.github.com>
2019-10-05 16:31:41 -0300

    Terminado el tp2

---
+++ TP2 Laboratorio/TP-02/Entidades/Dulce.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Dulce : Producto
+    {
+        public Dulce(EMarca marca, string patente, ConsoleColor color):base(marca,patente,color)
+        {
+        }
+
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        public override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }          
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : "+ this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
