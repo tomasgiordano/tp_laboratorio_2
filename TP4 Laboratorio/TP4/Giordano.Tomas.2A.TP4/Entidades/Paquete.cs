75118cc7d847857c3fb2de3c6cf35515486be85c
tomasgiordano <tomiigiordano@gmail.com>
2019-11-14 20:18:33 -0300

    empezando

---
+++ TP4 Laboratorio/TP4/Giordano.Tomas.2A.TP4/Entidades/Paquete.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Paquete:IMostrar<Paquete>
+    {
+        #region Campos
+        private string direccionEntrega;
+        private EEstado estado;
+        private string traquingID;
+        #endregion
+
+        #region Propiedades
+        public string DireccionEntrega
+        {
+            get { return this.direccionEntrega; }
+            set { this.direccionEntrega=value; }
+        }
+
+        public EEstado Estado
+        {
+            get { return this.estado; }
+            set { this.estado = value; }
+        }
+
+        public string TrackingID
+        {
+            get { return this.traquingID; }
+            set { this.traquingID = value; }
+        }
+        #endregion
+
+        #region Metodos
+        public void MockCicloDeVida()
+        {
+
+        }
+
+        public string MostrarDatos(IMostrar<Paquete> elemento)
+        {
+            return "";
+        }
+
+        //public static bool operator ==(Paquete p1, Paquete p2)
+        //{
+            
+        //}
+
+        //public static bool operator !=(Paquete p1, Paquete p2)
+        //{
+            
+        //}
+
+        public Paquete(string direccionEntrega, string trackingID)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        #endregion
+
+        #region Eventos
+        public event DelegadoEstado InformaEstado;
+        #endregion
+
+        #region Tipos anidados
+        public delegate void DelegadoEstado();
+
+        public enum EEstado
+        {
+            Ingresado,
+            EnViaje,
+            Entregado,
+        }
+        #endregion
+    }
+}
---
+++ TP4 Laboratorio/TP4/Giordano.Tomas.2A.TP4/Entidades/PaqueteDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Entidades
+{
+    public static class PaqueteDAO
+    {
+        #region Campos
+        public static SqlCommand comando;
+        public static SqlConnection conexion;
+        #endregion
+
+        #region Metodos
+        //public static bool Insertar(Paquete p)
+       // {
+
+        //}
+
+        static PaqueteDAO()
+        {
+            conexion = new SqlConnection(Properties.Settings.Default.Conexion);
+            comando = new SqlCommand(); 
+        }
+        #endregion
+    }
+}
