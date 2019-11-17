ff31605a289f48472f4494f5639afaba66e1f5ce
tomasgiordano <tomiigiordano@gmail.com>
2019-11-17 16:01:50 -0300

    Tests Unitarios terminados

---
+++ TP4 Laboratorio/TP4/Giordano.Tomas.2A.TP4/TestUnitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitarios")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("40f34b1a-8b6b-483d-b722-51b751cc368c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TP4 Laboratorio/TP4/Giordano.Tomas.2A.TP4/TestUnitarios/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+namespace TestUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ListaDePaquetesInstanciada()
+        {
+            Correo c = new Correo();
+            Assert.AreNotEqual(c.Paquetes, null);
+        }
+
+        [TestMethod]
+        public void PaquetesIgualesEnLista()
+        {
+            try
+            {
+                Correo c = new Correo();
+                Paquete p1 = new Paquete("Paquete.1", "123456789");
+                Paquete p2 = new Paquete("Paquete.2", "123456789");
+                c += p1;
+                c += p2;
+            }
+            catch (Exception error)
+            {
+                Assert.IsInstanceOfType(error, typeof(TrackingIdRepetidoException));
+            }
+        }
+    }
+}
